a2ccf7f455d86d425fed326d56a192ec23b59414
Oscar Valles <oscar@findyourpod.today>
2023-07-01 18:52:40 -0500

    two sum problem formatting

--- data_structures/two-sum-leetcode.py
+++ data_structures/two-sum-leetcode.py
@@ -1,9 +1,12 @@
 """
-This is the Python solution for the Two Sum problem in Leetcode
-Problem: Given an array of integers "nums" and an integer "target",
-         return indices of the two numbers such that they add up to target.
-         You may assume that each input would have exactly one solution,
-         and you may not use the same element twice.
+This is the Python solution for the Two Sum problem in
+        Leetcode
+Problem: Given an array of integers "nums" and
+         an integer "target", return indices of the
+         two numbers such that they add up to target.
+         You may assume that each input would have
+         exactly one solution, and you may not
+         use the same element twice.
 """
 
 
